cda25edcaabacb51134ff3429ea181e0d687ffcf
Dhiraj Phatake <80913995+dhirajphatake@users.noreply.github.com>
2024-01-04 14:06:49 +0000

    adding first file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 226204;
+let accountEmail = "dhirajphatake185@gmail.com";
+var accountPassword = "123";
+accountCity = "Ahmednagar";
+let accountState;
+
+// accountId = 12; //we cannot change const variables
+accountEmail = "dhiraj@gmail.com";
+accountPassword = "321";
+accountCity = "Pune";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope qand functional scope
+*/
+
+console.table([accountId ,accountEmail, accountPassword, accountCity, accountState])
