9cbc28cb5e18ef1543011ecc82a5ed1449593a14
Dhiraj Phatake <80913995+dhirajphatake@users.noreply.github.com>
2024-01-04 14:04:48 +0000

    premitive and non-premitive datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3); //we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3); //code readibility should be high
+
+console.log("Dhiraj");
+
+// datatyppes
+
+let name = "Dhiraj";
+let age = 22;
+let isLoggedIn = false;
+let state = null;
+
+
+// range 
+// premitive datatypes
+// number => 2 to the power of 53 
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value / representation of empty value
+// undefined => variables is declared but not asign value
+// symbol => find the uniqueness
+
+// non-premitive datatypes
+// object 
+
+// typeof
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
