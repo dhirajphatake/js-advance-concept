d044deaa3d2f253be4dc2a42d0f5b7f7c33c68fc
Dhiraj Phatake <80913995+dhirajphatake@users.noreply.github.com>
2024-01-08 20:10:40 +0000

    remaining conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,38 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+
+// Operations 
+let value = 3;
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello";
+let str2 = " Dhiraj";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("----------------------");
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //string aadhi asel tar bakila pan string madhe convert honar
+console.log(1 + 2 + "2"); //string nantar asel tar aadhiche operation honar 
+
+
+console.log("----------------------");
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+"");
